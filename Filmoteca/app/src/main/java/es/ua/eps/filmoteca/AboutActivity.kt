cce625129c5baf061904d4e986475271d90efb0d
Máster Móviles <mastermoviles@iMac.local>
2023-09-27 20:49:18 +0200

    ej 2 por mitad

--- Filmoteca/app/src/main/java/es/ua/eps/filmoteca/AboutActivity.kt
+++ Filmoteca/app/src/main/java/es/ua/eps/filmoteca/AboutActivity.kt
@@ -1,5 +1,7 @@
 package es.ua.eps.filmoteca
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,18 +14,26 @@ class AboutActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-            Toast.makeText(this, R.string.textError, Toast.LENGTH_LONG).show()
+            //Toast.makeText(this, R.string.textError, Toast.LENGTH_LONG).show()
+            val viewIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("http://www.google.com"))
+            startActivity(viewIntent)
 
         }
         val button2 = findViewById<Button>(R.id.button2)
         button2.setOnClickListener {
-            Toast.makeText(this, R.string.textError, Toast.LENGTH_LONG).show()
+            //Toast.makeText(this, R.string.textError, Toast.LENGTH_LONG).show()
+            val sendIntent = Intent(
+                Intent.ACTION_SENDTO,
+                Uri.parse("mailto:sergswa7@gmail.com")
+            )
+            startActivity(sendIntent)
 
         }
         val button3 = findViewById<Button>(R.id.button3)
         button3.setOnClickListener {
-            Toast.makeText(this, R.string.textError, Toast.LENGTH_LONG).show()
-
+            finish()
         }
 
     }
